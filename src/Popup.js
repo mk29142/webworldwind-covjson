330399b627f0422033333b9d1f7b951f8b1e73a6
mk29142 <mridul.kumar02@gmail.com>
2016-08-05 12:22:27 +0100

    Added class for popup display.

---
+++ src/Popup.js
@@ -0,0 +1,74 @@
+function Popup(wwd, cov, dom) {
+  this._wwd = wwd
+  this._cov = cov
+  this._dom = dom
+}
+
+Popup.prototype.display = function() {
+
+  var self = this
+
+  var cR = new WorldWind.ClickRecognizer(this._wwd, function() {
+    var xClick = cR.clientX
+    var yClick = cR.clientY
+
+    var lats = self._dom.axes.get('y').values
+    var lons = self._dom.axes.get('x').values
+
+    //world wind has bug, doesn't give right lon and lat when zoomed out
+    var vec = new WorldWind.Vec2(xClick, yClick)
+    var lat = wwd.pick(vec).terrainObject().position.latitude
+    var long = wwd.pick(vec).terrainObject().position.longitude
+
+    var iLat = CovUtils.indexOfNearest(lats, lat)
+    var iLon = CovUtils.indexOfNearest(lons, long)
+
+    var firstParamKey = self._cov.parameters.keys().next().value
+    var param = self._cov.parameters.get(firstParamKey)
+    var property = param.observedProperty.label.en
+    var categories = param.observedProperty.categories
+
+    var encoding = param.categoryEncoding
+
+    self._cov.loadRange(firstParamKey).then(function(range) {
+
+      var canvas = document.getElementById("canvas")
+      var popup = document.getElementById("popup")
+      var val = range.get({y: iLat, x: iLon})
+
+      if(categories) {
+        var keys = Array.from(encoding.keys())
+
+        for(var i = 0; i < keys.length; i++) {
+          var encodingVal = encoding.get(keys[i])
+          if(val == encodingVal) {
+            val = keys[i]
+            break
+          }
+        }
+
+        var newVal = categories.filter(function(obj) {
+          return obj.id == val
+        })
+        if(val) {
+          val = newVal[0].label.en
+        }
+      }
+
+      canvas.addEventListener("click", function(e) {
+        var xPosition = e.clientX - canvas.offsetLeft
+        var yPosition = e.clientY - canvas.offsetTop
+        popup.style.left = xPosition+'px'
+        popup.style.top = yPosition+'px'
+
+        var	txt = document.createTextNode(property + ": " + val)
+
+        if(!val) {
+          txt = document.createTextNode("No data")
+        }
+        popup.innerText = txt.textContent
+
+      })
+    })
+  })
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 
-
-
-
-
-
-
   (function () {
 
   // Create a World Window for the canvas.
@@ -21,64 +15,25 @@
 
   window.wwd = wwd
 
-
-  function getParameterByName(name, url) {
-      if (!url) url = window.location.href;
-      name = name.replace(/[\[\]]/g, "\\$&");
-      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-          results = regex.exec(url);
-      if (!results) return null;
-      if (!results[2]) return '';
-      return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
-
-  var uiManager
-  var file_name = getParameterByName('file_name');
-  console.log(file_name)
-  CovJSON.read("testdata/" + file_name).then(function (cov) {
+  //
+  // function getParameterByName(name, url) {
+  //     if (!url) url = window.location.href;
+  //     name = name.replace(/[\[\]]/g, "\\$&");
+  //     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+  //         results = regex.exec(url);
+  //     if (!results) return null;
+  //     if (!results[2]) return '';
+  //     return decodeURIComponent(results[2].replace(/\+/g, " "));
+  // }
+  //
+  // var file_name = getParameterByName('file_name');
+  // console.log(file_name)
+  CovJSON.read("testdata/grid.covjson").then(function (cov) {
 
     cov.loadDomain().then(function(dom) {
-      uiManager = new UIManager(wwd,cov,dom)
-      // var layer = uiManager.getLayer()
-      // console.log(layer);
-
-      // var proj = CovUtils.getProjection(dom)
-      // console.log(proj);
-
-      var cR = new WorldWind.ClickRecognizer(wwd, function() {
-        var xClick = cR.clientX
-        var yClick = cR.clientY
-
-        console.log(xClick + "  " + yClick);
-
-        var lats = dom.axes.get('y').values
-        var lons = dom.axes.get('x').values
-
-        var bbox = getGridBbox(dom.axes)
-        console.log(bbox);
-
-        //world wind has bug, doesn't give right lon and lat when zoomed out
-        var vec = new WorldWind.Vec2(xClick, yClick)
-        var lat = wwd.pick(vec).terrainObject().position.latitude
-        var long = wwd.pick(vec).terrainObject().position.longitude
-
-        var iLat = CovUtils.indexOfNearest(lats, lat)
-        var iLon = CovUtils.indexOfNearest(lons, long)
-
-        console.log(lat + "  " + long);
-          var firstParamKey = cov.parameters.keys().next().value
-          cov.loadRange(firstParamKey).then(function(range) {
-            console.log(range.get({y: iLat, x: iLon}))
-          })
-      })
-
-
-    //
-    //   var layer = CovJSONLayer(cov, {
-    //     paramKey: firstParamKey,
-    //   })
+      var uiManager = new UIManager(wwd,cov,dom)
+      var popup = new Popup(wwd, cov, dom).display()
 
     })
   })
 }())
-
